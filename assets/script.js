aa5af67363257c7d2c5b0e4ebd519328f432a1d9
relentlessreed <relentlessreed@gmail.com>
2021-04-03 01:35:42 -0500

    Added functions to diffent character types

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,27 @@
+// Beginning of code
+
+// Adding functions to diffent character types
+
+// Gets random lowercase letter from charcode
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+// Gets random uppercase letter from charcode
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+// Gets random number from charcode
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+// Gets random symbol from string
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
